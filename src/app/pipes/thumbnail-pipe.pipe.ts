dab91794cfb86e56ae3e1d34a63d5e2ceb93f28f
Ville Alla <villelohkovuori1@gmail.com>
2018-02-05 14:10:44 +0200

    done

--- src/app/pipes/thumbnail-pipe.pipe.ts
+++ src/app/pipes/thumbnail-pipe.pipe.ts
@@ -7,14 +7,24 @@ export class ThumbnailPipePipe implements PipeTransform {
 
   baseMediaUrl = 'http://media.mw.metropolia.fi/wbma/uploads/';
 
-  transform(value: any, size?: any): string {
+  transform(value: any, size?: string): string {
 
     const defaultSize = '-tn320.png';
+    let usedSize;
+
+    if (size === null || (size !== 'small' && size !== 'large') ) {
+      usedSize = defaultSize;
+    } else if (size === 'small') {
+      usedSize = '-tn160.png';
+    } else if (size === 'large') {
+      usedSize = '-tn640.png';
+    }
+
     let tempArray;
     let tempStr;
 
     tempArray = value.split('.'); // value = 'filename' attribute; result: [pic png]
     tempStr = tempArray[0]; // result: 'pic'
-    return this.baseMediaUrl + tempStr + (size || defaultSize); // replace original image url
+    return this.baseMediaUrl + tempStr + usedSize; // replace original image url
   }
 } // end class
